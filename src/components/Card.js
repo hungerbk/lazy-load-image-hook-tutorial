aef47b6fb3054d7ba73f165a410518977fedacf3
matt <wootae.jung@beautyselection.co.kr>
2023-11-14 17:30:01 +0900

    Card component lazy load image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
+  const imgRef = useRef();
+  // useEffect
+  useEffect(() => {
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // 이미지를 로딩
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(callback, {});
+    // 옵저버가 관찰
+    observer.observe(imgRef.current);
+    // 인털secting 될 때 callback
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="Card text-center">
-      <img src={props.image} />
+      <img data-src={props.image} ref={imgRef} alt="" />
       <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
         {props.children}
       </div>
